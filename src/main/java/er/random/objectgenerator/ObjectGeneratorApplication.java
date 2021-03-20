eb4af77bd3386a537297cec630c52197abd72964
Erik Aitkulov <eaitkulov@luxoft.com>
2021-03-20 17:43:15 +0300

    init

---
+++ src/main/java/er/random/objectgenerator/ObjectGeneratorApplication.java
@@ -0,0 +1,13 @@
+package er.random.objectgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObjectGeneratorApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ObjectGeneratorApplication.class, args);
+  }
+
+}
